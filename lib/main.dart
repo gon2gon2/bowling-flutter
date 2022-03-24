61fe65c807a0b8b3f586862bc43514437325bcb9
gon2gon2 <ark5354@gmail.com>
2022-03-25 00:37:39 +0900

    get request success

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'user.dart';
+
+Future<List<UserJ>> fetchPost() async {
+  final response = await http.get(Uri.parse("http://10.0.2.2:5000/"));
+
+  if (response.statusCode == 200) {
+    var jsonData = jsonDecode(utf8.decode(response.bodyBytes));
+    var result = jsonData.map<UserJ>((data) => UserJ.fromJson(data)).toList();
+    // print(result);
+    return result;
+  } else {
+    throw Exception('Failed to load users');
+  }
+}
+
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,6 +28,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // theme: ,
       home: DefaultTabController(
         length: 4,
         child: Scaffold(
@@ -39,45 +56,164 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-class FriendPage extends StatelessWidget {
+
+class FriendPage extends StatefulWidget {
   const FriendPage({Key? key}) : super(key: key);
 
   @override
+  State<FriendPage> createState() => _FriendPageState();
+}
+
+class _FriendPageState extends State<FriendPage> {
+  final List<User> _allUsers = [
+    User(userName: "XX현", userScoreList: [
+      89,
+      146,
+      141,
+      167,
+      168,
+      135,
+      194,
+      122,
+      143,
+      142,
+      188,
+      132,
+      179,
+      195,
+      160,
+      206,
+      179,
+      156,
+      212,
+      135,
+      126,
+      131,
+      167,
+      125,
+      161
+    ]),
+    User(userName: "XX준", userScoreList: [
+      90,
+      97,
+      83,
+      103,
+      143,
+      129,
+      101,
+      106,
+      95,
+      104,
+      114,
+      81,
+      100,
+      80,
+      132,
+      97,
+      97,
+      147,
+      79,
+      95,
+      86,
+      95,
+      85,
+      154,
+      113
+    ]),
+    User(userName: "XX곤", userScoreList: [
+      96,
+      78,
+      123,
+      92,
+      80,
+      100,
+      107,
+      107,
+      110,
+      100,
+      88,
+      82,
+      100,
+      109,
+      84,
+      91,
+      83,
+      113,
+      108,
+      76,
+      96,
+      81,
+      101,
+      89,
+      157
+    ]),
+    User(userName: "XX일", userScoreList: [
+      140,
+      91,
+      170,
+      112,
+      121,
+      139,
+      142,
+      132,
+      123,
+      136,
+      102,
+      172,
+      160,
+      200
+    ]),
+    User(userName: "XX석", userScoreList: [
+      116,
+      115,
+      93,
+      121,
+      96,
+      139,
+      131,
+      111,
+      115,
+      180,
+      127,
+      111,
+      137,
+      119,
+      88,
+      125,
+      103,
+    ])
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: const <Widget>[
-        Card(
-          child: ListTile(
-            leading: FlutterLogo(),
-            title: Text("XX현"),
+    return _allUsers.isNotEmpty
+        ? ListView.builder(
+            itemCount: _allUsers.length,
+            itemBuilder: (context, int index) {
+              return Card(
+                  child: ListTile(
+                title: Text(_allUsers[index].userName),
+                leading: const Icon(Icons.account_circle_sharp),
+                // onTap: () {
+                //   showDialog(
+                //       context: context,
+                //       builder: (BuildContext context) {
+                //         return AlertDialog(
+                //             title: Text('${_allUsers[index].userName}의 상세정보'),
+                //             content: SizedBox(
+                //               child: Column(children: [
+                //                 Text(('평균 점수: ${_allUsers[index].average}')),
+                //                 Text(('핸디캡: ${_allUsers[index].handicap}'))
+                //               ]),
+                //               height: 100,
+                //             ));
+                //       });
+                // },
+                onTap: fetchPost,
+              ));
+            },
           )
-         ),
-        Card(
-            child: ListTile(
-              leading: FlutterLogo(),
-              title: Text('XX준'),
-            )
-        ),
-        Card(
-            child: ListTile(
-              leading: FlutterLogo(),
-              title: Text('XX곤'),
-            )
-        ),
-        Card(
-            child: ListTile(
-              leading: FlutterLogo(),
-              title: Text('XX일'),
-            )
-        ),
-        Card(
-            child: ListTile(
-              leading: FlutterLogo(),
-              title: Text('XX석'),
-            )
-        ),
-      ]
-    );
+        : const Text("No items");
   }
 }
 
@@ -86,7 +222,7 @@ class GroupPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Text("그룹페이지에용"),
     );
   }
@@ -97,21 +233,19 @@ class SchedulePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Text("일정 페이지에용"),
     );
   }
 }
-
 
 class SettingPage extends StatelessWidget {
   const SettingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-     child: Text("설정페이지에용"),
+    return const Center(
+      child: Text("설정페이지에용"),
     );
   }
 }
-
--- lib/user.dart
+++ lib/user.dart
@@ -1,11 +1,42 @@
+import 'package:collection/collection.dart';
+
+const num standard = 140;
+const num ratio = 0.8;
+
 class User {
+  late String userName;
+  late List<int> userScoreList;
 
-  String _userName;
-  num _userAverage;
+  User({
+    required this.userName,
+    required this.userScoreList,
+  });
 
-  User({String name, num avg,}) {
-    _userName = name;
-    _userAverage = avg;
+  // 핸디캡 get 함수
+  num get average => (userScoreList.average.round());
+  num get handicap => ((standard - userScoreList.average) * ratio).round();
+}
+
+class UserJ {
+  final String userName;
+  final List<int> userScoreList;
+
+  UserJ({
+    required this.userName,
+    required this.userScoreList,
+  });
+
+  factory UserJ.fromJson(Map<String, dynamic> json) {
+    var userScoreFromJson = json['userScoreList'];
+    List<int> scoreList = List<int>.from(userScoreFromJson);
+    return UserJ(
+      userName: json['userName'],
+      userScoreList: scoreList,
+      
+    );
   }
 
+  // 핸디캡 get 함수
+  num get average => (userScoreList.average.round());
+  num get handicap => ((standard - userScoreList.average) * ratio).round();
 }
